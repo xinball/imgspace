fae6f697b5bcf733d087a44d5231bc86d193cb35
xinball <xinball@qq.com>
2021-01-06 16:47:06 +0800

    half

---
+++ app/src/main/java/top/xb/imgspace/view/NoScrollGridView.java
@@ -0,0 +1,42 @@
+package top.xb.imgspace.view;
+
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
+import androidx.annotation.RequiresApi;
+
+public class NoScrollGridView extends GridView {
+
+    public NoScrollGridView(Context context) {
+        super(context);
+    }
+
+    public NoScrollGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NoScrollGridView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public NoScrollGridView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    /**
+     * 设置不滚动
+     *
+     * 其中onMeasure函数决定了组件显示的高度与宽度；
+     * makeMeasureSpec函数中第一个函数决定布局空间的大小，第二个参数是布局模式
+     * MeasureSpec.AT_MOST的意思就是子控件需要多大的空间就扩展到多大的空间
+     * 同样的道理，ListView也适用
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+}
